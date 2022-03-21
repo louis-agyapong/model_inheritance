93cee442f0032bde4bd56597a48b7a2c46a728ee
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-21 01:36:45 +0000

    initial commit

---
+++ apps/quiz/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Quiz(models.Model):
+    """
+    Quiz model
+    """
+
+    name = models.CharField(_("Name"), max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    """
+    Question model
+    """
+
+    quiz = models.ForeignKey(Quiz, verbose_name=_("Quiz"), on_delete=models.CASCADE, related_name="questions")
+    text = models.CharField(_("Text"), max_length=255)
+    order = models.SmallIntegerField(_("Order"))
+
+    class Meta:
+        ordering = ("order",)
+        abstract = True
+
+    def __str__(self):
+        return self.text
